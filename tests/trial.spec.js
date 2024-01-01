0a8cdfea68d6bfa2741a1e2c81b3e4fc7d4dc65e
Aamir Maniar <aamironline@gmail.com>
2024-01-01 15:27:24 +0530

    Method names are now more inline with those in local and session storages

--- tests/trial.spec.js
+++ tests/trial.spec.js
@@ -3,7 +3,7 @@ import vault from '../dist/index.js';
 describe("A suite", () => {
   it("contains a spec with an expectation", async () => {
     vault.name = "John Doe2";
-    const value = await vault.name;
+    const value = await vault.getItem("name")
     expect(value).toBe("John Doe2");
 
     delete vault["name"];
