1d1faa9430688a5bf6b773a1288ebc1dee313f6a
Aamir Maniar <aamironline@gmail.com>
2024-02-03 13:33:31 +0530

    Refactor trial.spec.js

--- tests/trial.spec.js
+++ tests/trial.spec.js
@@ -1,18 +1,7 @@
-import vault from "../dist/index.js"
-import * as crypto from '../dist/crypto.js';
 
 describe("A suite", () => {
-  it("contains a spec with an expectation", async () => {
-  const secretData = 'Hello, World!';
-  const salt = await crypto.generateSalt("salt")
-  const key = await crypto.generateKey("password", salt);
-  const encryptedData = await crypto.encrypt(secretData, key);
-  const decrypted = await crypto.decrypt(encryptedData, key);
-
-  vault.encryptedData = encryptedData;
-
-  console.log('Encrypted Data:', new Uint8Array(encryptedData));
-  console.log('Decrypted Data:', decrypted);
+  it("wip tests useful during development", async () => {
+    // TODO: Write tests
   });
 })
 
