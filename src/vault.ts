e01c61fab5ae5bfeb6e399451a6e36b2de1f3e77
Aamir Maniar <aamironline@gmail.com>
2024-02-03 13:33:24 +0530

    Refactor Vault class constructor and methods

--- src/vault.ts
+++ src/vault.ts
@@ -1,3 +1,5 @@
+import proxyHandler from "./proxy-handler";
+
 const r = 'readonly', rw = 'readwrite';
 const s = 'store';
 
@@ -11,40 +13,41 @@ const s = 'store';
  * - You can store large amounts of data, not just 5MB.
  */
 export default class Vault {
-  #dbName = 'vault';
-  #db: IDBDatabase | null = null;
+  dbName = 'vault';
+  db: IDBDatabase | null = null;
 
   // Fake custom properties support. Custom properties are stored in the
   // indexdb as key/value pairs. This is a workaround to allow custom
   // properties to be set and retrieved as if they were native properties.
   [key: string]: any;
 
-  constructor(dbName?: string) {
-    this.#dbName = dbName || this.#dbName;
-    return new Proxy(this, proxyHandler);
+  constructor(dbName?: string, isParent: boolean = false) {
+    this.dbName = dbName || this.dbName;
+    // Use instanceToProxy if provided, otherwise default to this
+    if (!isParent) return new Proxy(this, proxyHandler)
   }
 
-  async setItem(key: string, value: any): Promise<void>   { this.do(rw, (s:any) => s.put({ key, value })) }
-  async getItem(key: string): Promise<any>                { this.do(r, (s: any) => s.get(key)).then((r:any) => r?.value ?? null) }
-  async removeItem(key: string):Promise<void>             { this.do(rw, (s:any) => s.delete(key)) }
-  async clear(): Promise<void>                            { this.do(rw, (s:any) => s.clear()) }
+  async setItem(key: string, value: any): Promise<void>   { return this.do(rw, (s:any) => s.put({ key, value })) }
+  async getItem(key: string): Promise<any>                { return this.do(r, (s: any) => s.get(key)).then((r:any) => r?.value ?? null) }
+  async removeItem(key: string):Promise<void>             { return this.do(rw, (s:any) => s.delete(key)) }
+  async clear(): Promise<void>                            { return this.do(rw, (s:any) => s.clear()) }
   async keys(): Promise<string[]>                         { return this.do(r, (s:any) => s.getAllKeys()) }
   async length(): Promise<number>                         { return this.do(r, (s:any) => s.count()) }
 
-  #init = async (): Promise<void> => {
+  async init(): Promise<void> {
     return new Promise((resolve, reject) => {
-      const request = indexedDB.open(this.#dbName, 1)
+      const request = indexedDB.open(this.dbName, 1)
       request.onupgradeneeded = (e:any) => {
         e.target.result.createObjectStore(s, { keyPath: 'key' })
       }
-      request.onsuccess = () => {this.#db = request.result;resolve()}
+      request.onsuccess = () => {this.db = request.result;resolve()}
       request.onerror = (e) => reject(e)
     })
   }
 
-  #do = async (operationType: IDBTransactionMode, operation: (store: IDBObjectStore) => IDBRequest): Promise<any> => {
-    if (!this.#db) await this.#init()
-    const transaction = this.#db!.transaction(s, operationType);
+  async do(operationType: IDBTransactionMode, operation: (store: IDBObjectStore) => IDBRequest): Promise<any> {
+    if (!this.db) await this.init()
+    const transaction = this.db!.transaction(s, operationType);
     const request     = operation(transaction.objectStore(s));
 
     return new Promise((resolve, reject) => {
@@ -54,8 +57,3 @@ export default class Vault {
   }
 }
 
-const proxyHandler = {
-  get(target: any, key: string)             { return (typeof target[key] === 'function') ? target[key].bind(target) : key in target ? target[key] : target.getItem(key) },
-  set(target: any, key: string, value: any) { return target.setItem(key, value) },
-  deleteProperty(target: any, key: string)  { return target.removeItem(key) }
-};
