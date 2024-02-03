01bc325feb222997bb6b0baa6a05e8b404819403
Aamir Maniar <aamironline@gmail.com>
2024-02-03 13:32:51 +0530

    Add customVault.spec.js unit test for Vault class

---
+++ tests/customVault.spec.js
@@ -0,0 +1,35 @@
+import Vault from "../dist/vault.js"
+
+describe("Vault", () => {
+  it("contains a Vault spec for creating custom vault store", async () => {
+    const vault = new Vault("test-store");
+
+    vault.firstName = "John";
+    const firstName = await vault.getItem("firstName");
+    expect(firstName).toBe("John");
+
+    vault.lastName = "Doe";
+    const lastName = await vault.getItem("lastName");
+    expect(lastName).toBe("Doe");
+
+    const keys = await vault.keys();
+    expect(keys).toEqual(["firstName", "lastName"]);
+
+    // Check length
+    expect(await vault.length()).toBe(2);
+
+    const length = await vault.length();
+    expect(length).toBe(2);
+
+    delete vault.firstName;
+    expect(await vault.firstName).toBe(null);
+
+    vault.clear();
+    expect(await vault.lastName).toBe(null);
+
+    expect(await vault.length()).toBe(0);
+    expect(await vault.keys()).toEqual([]);
+  });
+})
+
+
