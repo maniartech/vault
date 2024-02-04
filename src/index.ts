033e04007a09084cccd85cddc06157dffa256acd
Aamir Maniar <aamironline@gmail.com>
2024-02-04 21:32:57 +0530

    Comments update

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import Vault from './vault';
 
 /**
- * The default vault instance.
+ * The default vault storage instance that provides a convenient way to use the
+ * Vault without having to instantiate it manually. This instance is created
+ * by default when the module is imported.
+ *
+ * @type {Vault}
  */
 const vault = new Vault();
 
