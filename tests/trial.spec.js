c5ba3244f46833ff64563adc6c99d9ebbe3d0e7a
Aamir Maniar <aamironline@gmail.com>
2025-07-16 13:11:18 +0530

    test(trial): add beforeEach and afterEach hooks for vault cleanup

--- tests/trial.spec.js
+++ tests/trial.spec.js
@@ -1,7 +1,16 @@
 import vault from "../index.js";
 
-
 describe("A suite", () => {
+  beforeEach(async () => {
+    // Clear vault before each test
+    await vault.clear();
+  });
+
+  afterEach(async () => {
+    // Clean up after each test
+    await vault.clear();
+  });
+
   it("wip tests useful during development", async () => {
     // Run the work-in-progress tests here!
     vault.setItem("key", "value")
